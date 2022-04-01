316fdf9d8e27b6e4f693d665daf6b250cd67b362
RedRaptor10 <weiminh_lam@yahoo.com>
2022-03-31 19:48:33 -0500

    Add user tests

--- tests/users.test.js
+++ tests/users.test.js
@@ -2,29 +2,46 @@ const request = require('supertest');
 const express = require('express');
 const { startMongoServer, stopMongoServer } = require('./mongoConfigTesting');
 
+require('../passport');
+
+const indexRouter = require('../routes/index');
 const usersRouter = require('../routes/users');
 
 const app = express(); // Set up a separate express app
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/', usersRouter);
+app.use('/api', indexRouter);
+app.use('/api/users', usersRouter);
+
+let token;
 
 beforeAll(async () => {
     await startMongoServer();
 
     // Insert mock data
+
+    // Create User
     await request(app)
-        .post('/create')
-        .set('Accept', 'application/json')
-        .send({
-            username: 'test',
-            password: 'test',
-            firstName: 'test',
-            lastName: 'test',
-            public: true,
-            admin: false
-        });
+    .post('/api/users/create')
+    .send({
+        username: 'test',
+        password: 'test',
+        firstName: 'test',
+        lastName: 'test',
+        public: true,
+        admin: false
+    });
+
+    // Log In
+    const response = await request(app)
+    .post('/api/log-in')
+    .send({
+        username: 'test',
+        password: 'test'
+    });
+
+    token = response.body.token;
 });
 
 /* Template
@@ -33,18 +50,46 @@ test('This is a test', done => {
         .expect('Content-Type', /json/)
         .expect(200, done);
 
-async/await
 test('This is an async/await test', async () => {
     const response = await request(app).get('/');
     expect(response.headers['content-type']).toMatch(/json/);
 });*/
 
+// Get User
 test('GET /api/users', async () => {
-    const response = await request(app).get('/');
+    const response = await request(app).get('/api/users');
     expect(response.headers['content-type']).toMatch(/json/);
     expect(response.status).toEqual(200);
     expect(Array.isArray(response.body)).toBeTruthy();
     expect(response.body.length).toEqual(1);
+});
+
+// Get Users
+test('GET /api/users/:username', async () => {
+    const response = await request(app).get('/api/users/test');
+    expect(response.headers['content-type']).toMatch(/json/);
+    expect(response.status).toEqual(200);
+    expect(response.body.username).toEqual('test');
+});
+
+// Update User
+test('POST /api/users/:username/update', async () => {
+    const response = await request(app).post('/api/users/test/update')
+    .set('Authorization', 'Bearer ' + token)
+    .send({
+        username: 'test2',
+        password: 'test'
+    });
+    expect(response.headers['content-type']).toMatch(/json/);
+    expect(response.status).toEqual(200);
+    expect(response.body.user.username).toEqual('test2');
+});
+
+// Delete User
+test('POST /api/users/:username/delete', async () => {
+    const response = await request(app).post('/api/users/test/delete')
+    .set('Authorization', 'Bearer ' + token);
+    expect(response.body.message).toEqual('Success');
 });
 
 afterAll(async () => {
