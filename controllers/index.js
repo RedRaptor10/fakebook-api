a609cc99c33477b2e81e43b6750c8f90690635b4
RedRaptor10 <weiminh_lam@yahoo.com>
2022-04-05 09:33:24 -0500

    Add log-in form validation

--- controllers/index.js
+++ controllers/index.js
@@ -1,27 +1,39 @@
+const { body, validationResult } = require('express-validator');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const nconf = require('nconf');
 
 // Set up nconf
-nconf.argv().env().file({ file: '../config.json' });
+nconf.argv().env().file({ file: './config.json' });
 
 // Log In
 exports.logIn = [
+    // Validate and sanitize fields
+    body('email').trim().isLength({ min: 1 }).withMessage('Email required.').isEmail().withMessage('Not a valid email.').escape(),
+    body('password', 'Password required.').trim().isLength({ min: 1 }).escape(),
+
     // Process Log In
     (req, res, next) => {
-        // Authenticate with database (NOTE: { session: false } is passed so that the user isn't saved in the session.)
-        passport.authenticate('local', { session: false }, (error, user, info) => {
-            if (error || !user) {
-                return res.status(400).json({ user, error, info });
-            }
+        // Extract the validation errors from request
+        const errors = validationResult(req);
 
-            // Log the user in
-            req.login(user, { session: false }, (error) => {
-                if (error) { res.json(error); }
-                // Generate a signed JSON web token with the contents of the user object (NOTE: Access user info via req.user.info)
-                const token = jwt.sign({ info: user }, nconf.get('JWT_SECRET'), { expiresIn: '5m' });
-                return res.json({ user, token });
-            });
-        })(req, res);
+        if (!errors.isEmpty()) {
+            res.json({ errors: errors.array() });
+        } else {
+            // Authenticate with database (NOTE: { session: false } is passed so that the user isn't saved in the session.)
+            passport.authenticate('local', { session: false }, (error, user, info) => {
+                if (error || !user) {
+                    return res.status(400).json({ user, error, info });
+                }
+
+                // Log the user in
+                req.login(user, { session: false }, (error) => {
+                    if (error) { res.json(error); }
+                    // Generate a signed JSON web token with the contents of the user object (NOTE: Access user info via req.user.info)
+                    const token = jwt.sign({ info: user }, nconf.get('JWT_SECRET'), { expiresIn: '5m' });
+                    return res.json({ user, token });
+                });
+            })(req, res);
+        }
     }
 ];
